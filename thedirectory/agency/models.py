828be3beb749a55c64e35287f587bf9acf8c76aa
ashleyzawadi <zawadikibathi@gmail.com>
2019-09-23 17:25:37 +0300

    Add image settings

--- thedirectory/agency/models.py
+++ thedirectory/agency/models.py
@@ -4,16 +4,16 @@ from django.db import models
 
 class Agency(models.Model):
     agencyname = models.CharField(max_length = 200)
-    registration_no = models.IntegerField(max_length=200)
+    registration_no = models.IntegerField()
     agency_logo = models.ImageField(default='default.jpg')
 
    
 
 class Agent(models.Model):
     name = models.CharField(max_length = 100)
-    agency = models.ForeignKey(Agency, on_delete=models.Cascade, blank =True)
-    registration_no = models.IntegerField(max_length=100)
+    agency = models.ForeignKey(Agency,on_delete=models.CASCADE,blank =True)
+    registration_no = models.IntegerField()
     email = models.EmailField()
-    phone_no = models.IntegerField(max_length=10)
+    phone_no = models.IntegerField()
 
 
