20005c22dbfde3b117879006aaee8949d33b1a4c
ashleyzawadi <zawadikibathi@gmail.com>
2019-10-13 10:55:09 +0300

    Edit wsgi.py

--- thedirectory/thedirectory/settings.py
+++ thedirectory/thedirectory/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
